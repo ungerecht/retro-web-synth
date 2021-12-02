81de2a0baeffc853b361c2954ae0e1fd4d5686dd
ungerecht <kevinungerecht@gmail.com>
2021-12-02 12:04:24 -0800

    Add icons for waveforms and arrow buttons

---
+++ src/icons.tsx
@@ -0,0 +1,35 @@
+export const upIcon = (
+  <svg width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
+    <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+  </svg>
+);
+
+export const downIcon = (
+  <svg width="40" height="40" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+  </svg>
+);
+
+export const sineIcon = (
+  <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M16.5,21C13.5,21 12.31,16.76 11.05,12.28C10.14,9.04 9,5 7.5,5C4.11,5 4,11.93 4,12H2C2,11.63 2.06,3 7.5,3C10.5,3 11.71,7.25 12.97,11.74C13.83,14.8 15,19 16.5,19C19.94,19 20.03,12.07 20.03,12H22.03C22.03,12.37 21.97,21 16.5,21Z" />
+  </svg>
+);
+
+export const squareIcon = (
+  <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M2 2V12H4V4H11V22H22V12H20V20H13V2H2Z" />
+  </svg>
+);
+
+export const triangleIcon = (
+  <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M22 12L17 22L7.1 6.04L4.24 12H2L7 2L16.9 17.96L19.76 12H22Z" />
+  </svg>
+);
+
+export const sawtoothIcon = (
+  <svg width="24px" height="24px" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M11 22V6.83L2 16V13.17L13 2V17.17L22 8V10.83L11 22Z" />
+  </svg>
+);
