409c404a2d1e0e4b9677648e120f535d1b771146
ungerecht <kevinungerecht@gmail.com>
2021-12-02 12:10:54 -0800

    Add base components to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Keyboard from "./Keyboard";
+import "../fonts/PressStart2P.ttf";
+import "../styles/App.css";
+import OctaveSwitch from "./OctaveSwitch";
+import WaveformSwitch from "./WaveformSwitch";
+import Slider from "./Slider";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <div className="top-container">
+        <WaveformSwitch />
+        <div className="envelopes">
+          <Slider label="ATK" />
+          <Slider label="DEC" />
+          <Slider label="SUS" />
+          <Slider label="REL" />
+        </div>
+        <OctaveSwitch />
+      </div>
+      <div className="bottom-container">
+        <Keyboard />
+      </div>
+    </div>
+  );
 };
 
 export default App;
