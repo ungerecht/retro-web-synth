a477109c604dd400e224b222963cecacb11a9978
ungerecht <kevinungerecht@gmail.com>
2021-12-07 15:20:05 -0800

    Create EnvelopeSliders component

---
+++ src/components/EnvelopeSliders.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Slider from "./Slider";
+
+import "../styles/EnvelopeSliders.css";
+import { Envelope } from "../types";
+
+interface EnvelopeProps {
+  envelope: Envelope;
+  setEnvelope: (value: number, label: string) => void;
+}
+
+const EnvelopeSliders = ({ envelope, setEnvelope }: EnvelopeProps) => {
+  return (
+    <div className="envelopes">
+      <Slider
+        label="ATK"
+        max={2}
+        value={envelope.attack}
+        setEnvelope={setEnvelope}
+      />
+      <Slider
+        label="DEC"
+        max={2}
+        value={envelope.decay}
+        setEnvelope={setEnvelope}
+      />
+      <Slider
+        label="SUS"
+        max={1}
+        value={envelope.sustain}
+        setEnvelope={setEnvelope}
+      />
+      <Slider
+        label="REL"
+        max={5}
+        value={envelope.release}
+        setEnvelope={setEnvelope}
+      />
+    </div>
+  );
+};
+
+export default EnvelopeSliders;
