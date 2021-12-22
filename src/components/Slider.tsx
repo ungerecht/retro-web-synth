5056a5d1d0739fa0117b2df8e410fb9caafe8e62
ungerecht <kevinungerecht@gmail.com>
2021-12-21 16:34:49 -0800

    Change Slider to be drawn dynamically with svg

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,32 +1,122 @@
 import React from "react";
-import { Time } from "tone/build/esm/core/type/Units";
-import "../styles/Slider.css";
 
 interface SliderProps {
-  label: string;
+  min: number;
   max: number;
-  value: Time | undefined;
-  setEnvelope: (value: number, label: string) => void;
+  step: number;
+  value: number;
+  width: number;
+  height: number;
+  onValueChange: (value: number) => void;
 }
 
-const Slider = ({ label, max, value, setEnvelope }: SliderProps) => {
-  const onValueChange = (event: any) => {
-    setEnvelope(parseFloat(event.target.value), label);
+const Slider = ({
+  min,
+  max,
+  step,
+  value,
+  width,
+  height,
+  onValueChange,
+}: SliderProps) => {
+  const middle = width / 2;
+  const trackWidth = 10;
+  const trackHeight = height;
+  const barWidth = 40;
+  const barHeight = 12;
+
+  const onScroll = (event: any) => {
+    let newValue;
+    if (event.deltaY < 0) {
+      //scrolling up
+
+      if (value === max) return; //value is at max
+
+      //scroll faster holding shift
+      event.shiftKey
+        ? (newValue = value + step * 4)
+        : (newValue = value + step);
+
+      if (newValue > max) newValue = max; //set newValue to max if it went over
+    } else {
+      //scrolling down
+
+      if (value === min) return; //value is at min
+
+      //scroll faster holding shift
+      event.shiftKey
+        ? (newValue = value - step * 4)
+        : (newValue = value - step);
+
+      if (newValue < min) newValue = min; //set newValue to min if it went over
+    }
+    onValueChange(newValue);
   };
 
+  const valueToPercentage = (v: number, min: number, max: number) => {
+    return ((v - min) * 100) / (max - min);
+  };
+
+  const percentageToValue = (percentage: number, min: number, max: number) => {
+    return (percentage * (max - min)) / 100 + min;
+  };
+
+  const getBarCoordinates = (value: number) => {
+    const percentage = valueToPercentage(value, min, max);
+    const x = middle;
+    const y = percentageToValue(percentage, height - barHeight, 0);
+    return { x, y };
+  };
+
+  const barCoordinates = getBarCoordinates(value);
+
+  const drawTickCoordinates = () => {
+    let tickY = new Array(7);
+    for (let i = 0; i < tickY.length; i += 1) {
+      tickY[i] = percentageToValue(
+        valueToPercentage(i, 0, tickY.length - 1),
+        barHeight / 2,
+        height - barHeight / 2
+      );
+    }
+
+    let s = "";
+    for (let i = 0; i < tickY.length; i += 1) {
+      s += `M${2} ${tickY[i]} L ${width - 2} ${tickY[i]} `;
+    }
+    return s;
+  };
+
+  const tickLines: string = drawTickCoordinates();
+
   return (
-    <div className="slider-container">
-      <label>{label}</label>
-      <input
-        className="slider"
-        type="range"
-        min={0}
-        max={max}
-        step={0.001}
-        defaultValue={value?.toString() ?? "0"}
-        onChange={onValueChange}
+    <svg width={width} height={height} onWheel={onScroll}>
+      <path d={tickLines} stroke="white" strokeWidth={2} />
+      <rect
+        x={middle - trackWidth / 2}
+        y={0}
+        width={trackWidth}
+        height={trackHeight}
+        fill="black"
+        rx={6}
       />
-    </div>
+      <rect
+        x={barCoordinates.x - barWidth / 2}
+        y={barCoordinates.y}
+        width={barWidth}
+        height={barHeight}
+        fill="black"
+        rx={1}
+      />
+      <line
+        stroke="white"
+        strokeWidth={2}
+        x1={barCoordinates.x - barWidth / 2 + 2}
+        y1={barCoordinates.y + barHeight / 2}
+        x2={barCoordinates.x + barWidth / 2 - 2}
+        y2={barCoordinates.y + barHeight / 2}
+      />
+    </svg>
   );
 };
 
