52fc64accc8636c688f35d3372c9807b13bb5dc9
ungerecht <kevinungerecht@gmail.com>
2022-01-21 16:27:23 -0800

    Rename ReverbKnob to ReverbControls, Add wet knob

---
+++ src/components/ReverbControls.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Knob from "./Knob";
+import "../styles/ReverbControls.css";
+
+interface ReverbProps {
+  reverb: { decay: number; wet: number };
+  setReverbDecay: (decay: number) => void;
+  setReverbWet: (wet: number) => void;
+}
+
+const ReverbControls = ({
+  reverb,
+  setReverbDecay,
+  setReverbWet,
+}: ReverbProps) => {
+  return (
+    <div className="reverb-controls-container">
+      <label>REVERB</label>
+      <div className="reverb-knobs-container">
+        <div className="reverb-wet-knob">
+          <label>WET</label>
+          <Knob
+            min={0}
+            max={1}
+            value={reverb.wet}
+            onValueChange={setReverbWet}
+            width={50}
+            height={50}
+            step={0.1}
+          />
+          <p>{`${(reverb.wet * 100).toFixed(0)}%`}</p>
+        </div>
+        <div className="reverb-decay-knob">
+          <label>DECAY</label>
+          <Knob
+            min={1}
+            max={30}
+            value={reverb.decay}
+            onValueChange={setReverbDecay}
+            width={50}
+            height={50}
+            step={1}
+          />
+          <p>{`${reverb.decay}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReverbControls;
