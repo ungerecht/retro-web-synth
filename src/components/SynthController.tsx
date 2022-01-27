bfe991ee1bf1656e6d936cd68f00e95955674301
ungerecht <kevinungerecht@gmail.com>
2022-01-26 16:07:07 -0800

    Add EQ3

--- src/components/SynthController.tsx
+++ src/components/SynthController.tsx
@@ -12,6 +12,7 @@ import * as Tone from "tone";
 
 import { KEY_TO_NOTE, NOTE_TO_KEY, VALID_KEYS } from "../globals/constants";
 import "../styles/SynthController.css";
+import EQ3Controls from "./EQ3Controls";
 
 type SynthState = {
   waveform: OscillatorType;
@@ -21,6 +22,13 @@ type SynthState = {
   pressedKeys: string[];
   volume: number;
   reverb: { decay: number; wet: number };
+  eq3: {
+    low: number;
+    mid: number;
+    high: number;
+    lowFrequency: number;
+    highFrequency: number;
+  };
 };
 
 class SynthController extends React.Component<{}, SynthState> {
@@ -28,6 +36,7 @@ class SynthController extends React.Component<{}, SynthState> {
   filter: Tone.Filter;
   masterVolume: Tone.Volume;
   reverb: Tone.Reverb;
+  EQ3: Tone.EQ3;
   state: SynthState;
   constructor(props: any) {
     super(props);
@@ -36,6 +45,7 @@ class SynthController extends React.Component<{}, SynthState> {
     this.filter = new Tone.Filter();
     this.masterVolume = new Tone.Volume(-10);
     this.reverb = new Tone.Reverb();
+    this.EQ3 = new Tone.EQ3();
 
     this.state = {
       waveform: "sine",
@@ -59,6 +69,13 @@ class SynthController extends React.Component<{}, SynthState> {
       reverb: {
         decay: 1, // 1 - 30
         wet: 0, // 0 - 1
+      },
+      eq3: {
+        low: 0,
+        mid: 0,
+        high: 0,
+        lowFrequency: 400,
+        highFrequency: 2500,
       },
     };
   }
@@ -90,9 +107,15 @@ class SynthController extends React.Component<{}, SynthState> {
     //set reverb options to default
     this.reverb.set(this.state.reverb);
 
-    //connect synth -> filter -> master volume -> output
+    //set EQ3 options to default
+    this.EQ3.set(this.state.eq3);
+
+    console.log(this.EQ3);
+
+    //connect synth -> filter -> EQ3 -> reverb -> master volume -> output
     this.synth.chain(
       this.filter,
+      this.EQ3,
       this.reverb,
       this.masterVolume,
       Tone.Destination
@@ -334,6 +357,61 @@ class SynthController extends React.Component<{}, SynthState> {
     this.reverb.set({ wet });
   };
 
+  setEQ3Low = (low: number) => {
+    low = Math.round(low);
+    this.setState((prevState) => ({
+      eq3: {
+        ...prevState.eq3,
+        low,
+      },
+    }));
+    this.EQ3.set({ low });
+  };
+
+  setEQ3Mid = (mid: number) => {
+    mid = Math.round(mid);
+    this.setState((prevState) => ({
+      eq3: {
+        ...prevState.eq3,
+        mid,
+      },
+    }));
+    this.EQ3.set({ mid });
+  };
+
+  setEQ3High = (high: number) => {
+    high = Math.round(high);
+    this.setState((prevState) => ({
+      eq3: {
+        ...prevState.eq3,
+        high,
+      },
+    }));
+    this.EQ3.set({ high });
+  };
+
+  setEQ3LowFrequency = (lowFrequency: number) => {
+    lowFrequency = Math.round(lowFrequency);
+    this.setState((prevState) => ({
+      eq3: {
+        ...prevState.eq3,
+        lowFrequency,
+      },
+    }));
+    this.EQ3.set({ lowFrequency });
+  };
+
+  setEQ3HighFrequency = (highFrequency: number) => {
+    highFrequency = Math.round(highFrequency);
+    this.setState((prevState) => ({
+      eq3: {
+        ...prevState.eq3,
+        highFrequency,
+      },
+    }));
+    this.EQ3.set({ highFrequency });
+  };
+
   render() {
     return (
       <div className="container">
@@ -348,7 +426,6 @@ class SynthController extends React.Component<{}, SynthState> {
             waveform={this.state.waveform}
             setWaveform={this.setWaveform}
           />
-
           <EnvelopeSliders
             envelope={this.state.envelope}
             setAttack={this.setEnvelopeAttack}
@@ -356,7 +433,6 @@ class SynthController extends React.Component<{}, SynthState> {
             setSustain={this.setEnvelopeSustain}
             setRelease={this.setEnvelopeRelease}
           />
-
           <FilterControls
             filterState={this.state.filter}
             filter={this.filter}
@@ -366,6 +442,14 @@ class SynthController extends React.Component<{}, SynthState> {
             setFilterDetune={this.setFilterDetune}
             setFilterGain={this.setFilterGain}
             setFilterFrequency={this.setFilterFrequency}
+          />
+          <EQ3Controls
+            eq3={this.state.eq3}
+            setEQ3Low={this.setEQ3Low}
+            setEQ3Mid={this.setEQ3Mid}
+            setEQ3High={this.setEQ3High}
+            setEQ3LowFrequency={this.setEQ3LowFrequency}
+            setEQ3HighFrequency={this.setEQ3HighFrequency}
           />
         </div>
         <div className="bottom-container">
