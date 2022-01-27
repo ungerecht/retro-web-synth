79a21e7015133b25c913ab1c78dedef2914277ec
ungerecht <kevinungerecht@gmail.com>
2022-01-26 20:07:44 -0800

    Implement Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import * as Icons from "../icons";
+import "../styles/Button.css";
 
 interface ButtonProps {
-  onValueChange: () => void;
+  value: string;
+  selected: string;
+  onValueChange: (event: any) => void;
 }
 
-const Button = () => {};
+const Button = ({ value, selected, onValueChange }: ButtonProps) => {
+  const icon = Icons[value as keyof typeof Icons];
+  return (
+    <React.Fragment>
+      <input
+        type="radio"
+        value={value}
+        id={value}
+        checked={value === selected}
+        onChange={onValueChange}
+        className="radio-button"
+      />
+      {icon ? (
+        <label className="radio-icon" htmlFor={value}>
+          {icon}
+        </label>
+      ) : (
+        <label className="radio-icon unselectable" htmlFor={value}>
+          {value}
+        </label>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Button;
