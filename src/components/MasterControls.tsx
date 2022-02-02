a5bc64fe51563aafddc2eb11d02f0e2bfc18a022
ungerecht <kevinungerecht@gmail.com>
2022-02-01 18:18:34 -0800

    Rename OctaveSwitch to MasterControls, add master vol

---
+++ src/components/MasterControls.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Knob from "./Knob";
+import "../styles/MasterControls.css";
+import { left, right } from "../icons";
+
+interface MasterControlsProps {
+  volume: number;
+  octave: number;
+  setOctave: (octave: number) => void;
+  setVolume: (value: number) => void;
+}
+
+const OctaveSwitch = ({
+  octave,
+  volume,
+  setOctave,
+  setVolume,
+}: MasterControlsProps) => {
+  console.log("render octave");
+  return (
+    <div className="master-controls-container">
+      <div className="column hasTooltip">
+        <Knob
+          min={-60}
+          max={0}
+          value={volume}
+          onValueChange={(value) => {
+            setVolume(value);
+          }}
+          width={50}
+          height={50}
+          step={1}
+        />
+        <label className="unselectable title-small">Volume</label>
+        <span className="tooltip value">{`${volume}db`}</span>
+      </div>
+      <div className="column">
+        <div className="row justify-between">
+          <button
+            className="octave-button"
+            onClick={() => {
+              setOctave(octave - 1);
+            }}
+            disabled={octave === 0}
+          >
+            {left}
+          </button>
+          <p className="unselectable octave">{octave}</p>
+          <button
+            className="octave-button"
+            onClick={() => {
+              setOctave(octave + 1);
+            }}
+            disabled={octave === 9}
+          >
+            {right}
+          </button>
+        </div>
+        <label className="unselectable title-small">Octave</label>
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(OctaveSwitch);
