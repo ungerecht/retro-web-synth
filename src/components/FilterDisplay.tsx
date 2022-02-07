74432277a1ea7e928bc7d6a717a15676b2f91cb8
ungerecht <kevinungerecht@gmail.com>
2022-02-07 11:45:44 -0800

    Fix canvas initial render with useEffect

--- src/components/FilterDisplay.tsx
+++ src/components/FilterDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { FilterRollOff, Filter } from "tone";
 
 import "../styles/FilterDisplay.css";
@@ -16,78 +16,84 @@ interface FilterDisplayProps {
 const FilterDisplay = ({ filterOptions }: FilterDisplayProps) => {
   const width = 256;
   const height = 156;
-  const filter = new Filter(filterOptions);
-  const response = filter.getFrequencyResponse(width);
-  const canvas = useRef<HTMLCanvasElement>(null);
-  const context = canvas.current?.getContext("2d");
-  const middle = height / 2;
   const dbScale = 40;
-  const pixelsPerDb = middle / dbScale;
+  const canvas = useRef<HTMLCanvasElement>(null);
 
-  const getXofFrequency = (freq: number) => {
-    return width * Math.sqrt((freq - 20) / (20000 - 20));
-  };
+  useEffect(() => {
+    const filter = new Filter(filterOptions);
+    const response = filter.getFrequencyResponse(width);
+    const middle = height / 2;
+    const pixelsPerDb = middle / dbScale;
+    const context = canvas.current?.getContext("2d");
+    if (context) {
+      //draw box
+      context.imageSmoothingEnabled = false;
+      context.fillStyle = "black";
+      context.fillRect(0, 0, width, height);
 
-  const dbToY = (db: number) => {
-    return middle - pixelsPerDb * db;
-  };
+      //draw frequency scale
+      context.lineWidth = 1;
+      context.strokeStyle = "gray";
+      context.beginPath();
+      for (let i = 100; i <= 10000; i = i * 10) {
+        const freqX = getXofFrequency(i, width);
+        context.moveTo(freqX, 0);
+        context.lineTo(freqX, height - 12);
+        context.stroke();
 
-  if (context) {
-    //draw box
-    context.imageSmoothingEnabled = false;
-    context.fillStyle = "black";
-    context.fillRect(0, 0, width, height);
-
-    //draw frequency scale
-    context.lineWidth = 1;
-    context.strokeStyle = "gray";
-    context.beginPath();
-    for (let i = 100; i <= 10000; i = i * 10) {
-      const freqX = getXofFrequency(i);
-      context.moveTo(freqX, 0);
-      context.lineTo(freqX, height - 12);
-      context.stroke();
-
-      let unit = "Hz";
-      let value = i;
-      if (i >= 1000) {
-        value = value / 1000;
-        unit = "KHz";
+        let unit = "Hz";
+        let value = i;
+        if (i >= 1000) {
+          value = value / 1000;
+          unit = "KHz";
+        }
+        context.font = "9px Helvetica";
+        context.fillStyle = "lightgray";
+        context.textAlign = "center";
+        context.fillText(value + unit, freqX, height - 3);
       }
-      context.font = "9px Helvetica";
-      context.fillStyle = "lightgray";
-      context.textAlign = "center";
-      context.fillText(value + unit, freqX, height - 3);
-    }
 
-    // //draw dbScale
-    context.lineWidth = 1;
-    context.strokeStyle = "gray";
-    context.beginPath();
-    for (let i = -dbScale + 10; i < dbScale; i = i + 10) {
-      let y = dbToY(i);
-      context.moveTo(0, y);
-      context.lineTo(width, y);
+      // //draw dbScale
+      context.lineWidth = 1;
+      context.strokeStyle = "gray";
+      context.beginPath();
+      for (let i = -dbScale + 10; i < dbScale; i = i + 10) {
+        let y = dbToY(i, middle, pixelsPerDb);
+        context.moveTo(0, y);
+        context.lineTo(width, y);
+        context.stroke();
+      }
+
+      //draw frequency response graph
+      context.strokeStyle = "cyan";
+      context.lineWidth = 2;
+      context.beginPath();
+
+      for (let i = 0; i < width; i++) {
+        const db = (20 * Math.log(response[i])) / Math.LN10;
+        context.lineTo(i, dbToY(db, middle, pixelsPerDb));
+      }
       context.stroke();
     }
 
-    //draw frequency response graph
-    context.strokeStyle = "cyan";
-    context.lineWidth = 2;
-    context.beginPath();
-
-    for (let i = 0; i < width; i++) {
-      const db = (20 * Math.log(response[i])) / Math.LN10;
-      context.lineTo(i, dbToY(db));
-    }
-    context.stroke();
-  }
+    return () => {
+      filter.dispose();
+    };
+  }, [filterOptions]);
 
   return (
     <div className="filter-display-container">
       <canvas ref={canvas} width={width} height={height} />
     </div>
   );
+};
+
+const getXofFrequency = (freq: number, width: number) => {
+  return width * Math.sqrt((freq - 20) / (20000 - 20));
+};
+
+const dbToY = (db: number, middle: number, pixelsPerDb: number) => {
+  return middle - pixelsPerDb * db;
 };
 
 export default React.memo(FilterDisplay);
