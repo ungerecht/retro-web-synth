04b3ba4b408e977cee111b7a38268f9772ec41e1
ungerecht <kevinungerecht@gmail.com>
2022-02-09 18:57:10 -0800

    Wrap children in div

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import SynthController from "./SynthController";
-
 import "../fonts/PressStart2P.ttf";
 import "../styles/App.css";
 
 const App = () => {
-  return <SynthController />;
+  return (
+    <div className="App">
+      <SynthController />
+    </div>
+  );
 };
 
 export default App;
