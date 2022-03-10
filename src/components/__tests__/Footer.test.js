0e8cb64a72880e72e6dc5bd1d7e67b5e198caede
ungerecht <kevinungerecht@gmail.com>
2022-03-09 18:53:07 -0800

    Add tests for Footer component

---
+++ src/components/__tests__/Footer.test.js
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Footer from "../Footer";
+
+describe("Footer component", () => {
+  test("it contains the title", () => {
+    const { getByText } = render(<Footer />);
+    expect(getByText("RETRO SYNTH")).toBeInTheDocument();
+  });
+  test("it contains the tagline", () => {
+    const { getByText } = render(<Footer />);
+    expect(getByText("An open-source web synthesizer")).toBeInTheDocument();
+  });
+  test("it contains a link to my github", () => {
+    const { getAllByRole } = render(<Footer />);
+    expect(getAllByRole("link")[0]).toHaveAttribute(
+      "href",
+      "https://github.com/ungerecht"
+    );
+  });
+  test("it contains a link to the github repo", () => {
+    const { getAllByRole } = render(<Footer />);
+    expect(getAllByRole("link")[1]).toHaveAttribute(
+      "href",
+      "https://github.com/ungerecht/retro-web-synth"
+    );
+  });
+});
