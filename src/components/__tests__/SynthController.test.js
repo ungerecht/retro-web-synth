3ddfcbda91549baaf9c3a7c37a92187bcc7a1120
ungerecht <kevinungerecht@gmail.com>
2022-03-16 19:24:02 -0700

    Create testing file for SynthController

---
+++ src/components/__tests__/SynthController.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import SynthController from "../SynthController";
+
+describe("SynthController component", () => {
+  test("", () => {});
+});
