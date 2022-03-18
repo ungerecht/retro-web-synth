bee11a876a89bdf427280feaff5e8f938afda2e5
ungerecht <kevinungerecht@gmail.com>
2022-03-17 20:01:41 -0700

    Add renders without crashing test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,6 +6,10 @@ import App from "../App";
 jest.mock("../SynthController");
 
 describe("App component", () => {
+  test("it renders without crashing", () => {
+    render(<App />);
+  });
+
   test("it renders the warning message", () => {
     const { getByText } = render(<App />);
     expect(getByText("Please switch to landscape mode")).toBeInTheDocument();
