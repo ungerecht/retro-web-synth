7dd2ba78353d62ba816f8ceb28ac84955af049a8
ungerecht <kevinungerecht@gmail.com>
2022-03-17 20:02:00 -0700

    Add whitespace

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -8,10 +8,12 @@ describe("Footer component", () => {
     const { getByText } = render(<Footer />);
     expect(getByText("RETRO SYNTH")).toBeInTheDocument();
   });
+
   test("it contains the tagline", () => {
     const { getByText } = render(<Footer />);
     expect(getByText("An open-source web synthesizer")).toBeInTheDocument();
   });
+
   test("it contains a link to my github", () => {
     const { getAllByRole } = render(<Footer />);
     expect(getAllByRole("link")[0]).toHaveAttribute(
@@ -19,6 +21,7 @@ describe("Footer component", () => {
       "https://github.com/ungerecht"
     );
   });
+
   test("it contains a link to the github repo", () => {
     const { getAllByRole } = render(<Footer />);
     expect(getAllByRole("link")[1]).toHaveAttribute(
