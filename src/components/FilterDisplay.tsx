c1cdade880154b21b95a30afc8c81cc3ede2ea01
ungerecht <kevinungerecht@gmail.com>
2022-05-16 23:52:53 -0700

    Move visualiser, small updates

--- src/components/FilterDisplay.tsx
+++ src/components/FilterDisplay.tsx
@@ -1,25 +1,82 @@
-import { useEffect, useRef, memo } from "react";
-import { context, Filter } from "tone";
+import { useEffect, useRef, memo, useState } from "react";
+import { context } from "tone";
 import { FilterDisplayProps, filterOptions } from "../types";
 import { getXofFrequency, getFrequencyOfX, dbToY } from "../utils";
 import "../styles/FilterDisplay.css";
 
-const FilterDisplay = ({ filterOptions }: FilterDisplayProps) => {
+const FilterDisplay = ({
+  filterOptions,
+  isPlaying,
+  fft,
+}: FilterDisplayProps) => {
   const width = 256;
   const height = 156;
   const dbScale = 40;
   const canvas = useRef<HTMLCanvasElement>(null);
 
+  const canvas2 = useRef<HTMLCanvasElement>(null);
+  const [audioData, setAudioData] = useState<Float32Array>();
+  const rafId = useRef<number>(0);
+
   useEffect(() => {
-    const response = getFreqResponse(width, filterOptions);
+    const tick = () => {
+      const data = fft.getValue();
+      setAudioData(data);
+      rafId.current = requestAnimationFrame(tick);
+    };
+    if (isPlaying) rafId.current = requestAnimationFrame(tick);
+    else {
+      const context = canvas2.current?.getContext("2d");
+      if (context) {
+        context.clearRect(0, 0, width, height);
+      }
+    }
+
+    return () => {
+      cancelAnimationFrame(rafId.current);
+    };
+  }, [fft, isPlaying]);
+
+  useEffect(() => {
+    const draw = () => {
+      const middle = height / 2;
+      const pixelsPerDb = middle / dbScale;
+      const context = canvas2.current?.getContext("2d");
+      if (context && audioData) {
+        context.lineWidth = 1;
+        context.clearRect(0, 0, width, height);
+        let path = new Path2D();
+        path.moveTo(0, height);
+        for (let i = 0; i < audioData.length; i++) {
+          const db = audioData[i] + dbScale;
+          const y = dbToY(db, middle, pixelsPerDb);
+          let x = getXofFrequency(fft.getFrequencyOfIndex(i), width);
+          if (x < 0) x = 0;
+          path.lineTo(x, y);
+        }
+        path.lineTo(width, height);
+        path.lineTo(0, height);
+        path.closePath();
+        const gradient = context.createLinearGradient(0, 0, width, 0);
+        gradient.addColorStop(0, "darkblue");
+        gradient.addColorStop(0.5, "blue");
+        gradient.addColorStop(0.95, "deepskyblue");
+        context.fillStyle = gradient;
+        context.fill(path);
+      }
+    };
+    draw();
+  }, [audioData, fft]);
+
+  useEffect(() => {
     const middle = height / 2;
     const pixelsPerDb = middle / dbScale;
+    const response = getFreqResponse(width, filterOptions);
     const context = canvas.current?.getContext("2d");
     if (context) {
       //draw box
       context.imageSmoothingEnabled = false;
-      context.fillStyle = "black";
-      context.fillRect(0, 0, width, height);
+      context.clearRect(0, 0, width, height);
 
       //draw frequency scale
       context.lineWidth = 1;
@@ -60,7 +117,7 @@ const FilterDisplay = ({ filterOptions }: FilterDisplayProps) => {
       context.beginPath();
 
       for (let i = 0; i < width; i++) {
-        const db = 20 * Math.log10(response[i]);
+        const db = dbScale * Math.log10(response[i]);
         context.lineTo(i, dbToY(db, middle, pixelsPerDb));
       }
       context.stroke();
@@ -69,7 +126,19 @@ const FilterDisplay = ({ filterOptions }: FilterDisplayProps) => {
 
   return (
     <div className="filter-display-container">
-      <canvas ref={canvas} width={width} height={height} />
+      <div className="holder" />
+      <canvas
+        className="filter-display"
+        ref={canvas}
+        width={width}
+        height={height}
+      />
+      <canvas
+        className="visualiser"
+        ref={canvas2}
+        width={width}
+        height={height}
+      />
     </div>
   );
 };
@@ -80,6 +149,7 @@ const getFreqResponse = (len: number, filterOptions: filterOptions) => {
     freqValues[i] = getFrequencyOfX(i, len);
   }
 
+  //setup filter clone and get frequency response
   const magValues = new Float32Array(len);
   const phaseValues = new Float32Array(len);
   const filterClone = context.createBiquadFilter();
