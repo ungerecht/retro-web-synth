5e1fb3fdc63b0a10308cefd4da1ae0afb9db2194
ungerecht <kevinungerecht@gmail.com>
2022-06-01 16:54:13 -0700

    Create Presets component

---
+++ src/components/Presets.tsx
@@ -0,0 +1,32 @@
+import React, { useState, ChangeEvent } from "react";
+
+import { PresetsProps } from "../types";
+import { defaults, supersaw } from "../presets";
+
+const Presets = ({ changePreset }: PresetsProps) => {
+  const [selected, setSelected] = useState(defaults.name);
+  const presets = [defaults, supersaw];
+
+  const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    const name = event.target.value;
+    const preset = presets.find((preset) => preset.name === name);
+    if (preset) changePreset(preset);
+    setSelected(name);
+  };
+
+  return (
+    <div className="presets-container">
+      <select className="preset-select unselectable" onChange={handleSelect}>
+        {presets.map((preset) => {
+          return (
+            <option key={preset.name} value={preset.name}>
+              {preset.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default Presets;
