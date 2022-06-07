c282c54cb1fc4d266cd5b89749e9102a459fc7ca
ungerecht <kevinungerecht@gmail.com>
2022-06-07 16:50:20 -0700

    cleanup

--- src/components/Presets.tsx
+++ src/components/Presets.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 
 import { PresetsProps } from "../types";
 import { defaults, supersaw, waterTemple, sleepy } from "../presets";
